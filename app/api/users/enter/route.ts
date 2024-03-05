799209b80c72a119ec35ad4fd29f4a533efa423c
skyeome <tjru7230@naver.com>
2024-03-05 21:16:41 +0900

    Feat: Access token 1차 설정

--- app/api/users/enter/route.ts
+++ app/api/users/enter/route.ts
@@ -1,8 +1,28 @@
-import { NextResponse } from "next/server";
+import client from "@/libs/server/client";
 
 export async function POST(request: Request) {
-  const res = await request.json();
-  return new Response(JSON.stringify(res), {
+  const { phone, email } = await request.json();
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return new Response("정보가 없습니다.", { status: 400 });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+  console.log(token);
+  return new Response(JSON.stringify({ phone, email }), {
     status: 200,
   });
 }
